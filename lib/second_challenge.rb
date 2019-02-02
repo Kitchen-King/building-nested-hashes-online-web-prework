ca5d20a8adb10376da35150b07a69a81ba3cd85c
Kyle Spencer <spencerk2@vcu.edu>
2019-02-02 23:38:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,14 +6,14 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+    }, 
     
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
+    }
   }
 
   
